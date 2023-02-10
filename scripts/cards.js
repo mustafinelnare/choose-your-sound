20be6cbed901c014c4cc26d3d9e1fe0ebf0f842c
Эльнар Мустафин <mustafinelnare@gmail.com>
2023-02-10 22:49:10 +0400

    feat add adaptive and js script

---
+++ scripts/cards.js
@@ -0,0 +1,62 @@
+const albumCovers = [
+  {
+    title: 'Maple Syrup',
+    subtitle: 'Barca',
+    image: 'https://i.postimg.cc/BQ3snspp/maplesyrup.jpg',
+    link: 'https://open.spotify.com/album/26VgzLAyBkVIn8SesEB6oB?si=Q-TsDWcCRJGWjbYRCT4kuQ',
+  },
+  {
+    title: 'yeyts',
+    subtitle: 'seat back',
+    image: 'https://i.postimg.cc/fL0ZJR12/yeyts.jpg',
+    link: 'https://open.spotify.com/album/7xc1FPjfJ5Fcfb58AcTGGb?si=wLax9T1PSFe3eKHPOAh-bQ',
+  },
+  {
+    title: 'Kick a Dope Verse',
+    subtitle: 'Uptown Tape',
+    image: 'https://i.postimg.cc/PrMHLcFj/kickadopeverse.jpg',
+    link: 'https://open.spotify.com/album/0YqYAE4LCjB610feWC0OpK?si=rJ_pUg-yRemquUrnmz7osQ',
+  },
+  {
+    title: 'igory',
+    subtitle: 'ocean view',
+    image: 'https://i.postimg.cc/bvrqfTgj/igory.jpg',
+    link: 'https://open.spotify.com/album/6kucfOGZ2dGI4G2rqqAW31?si=zA3E6jUYQ5emNhY5TCp-6g',
+  },
+  {
+    title: 'J. Rawls',
+    subtitle: '#jazzhop',
+    image: 'https://i.postimg.cc/Bb93DG0q/jrawls.jpg',
+    link: 'https://open.spotify.com/album/0CzzYYa80De9BaThHIE9mR?si=TnYphb0sQN2D_eFxLPmSOg',
+  },
+  {
+    title: 'John Da Lemon',
+    subtitle: 'Organic Ambience',
+    image: 'https://i.postimg.cc/j2Hb4kxy/johndalemon.jpg',
+    link: 'https://open.spotify.com/album/6Cns4acwXbmLeqrxAbULeq?si=K_fohlLST_GffWzQBeFoAw',
+  },
+  {
+    title: 'Boora',
+    subtitle: 'Soundogolism 2',
+    image: 'https://i.postimg.cc/gc8mprVr/boora.jpg',
+    link: 'https://open.spotify.com/album/78dbvKxJ5maCEzjfrkVV3b?si=5EwtBsCOSVKQFsuvbbqWTg',
+  },
+  {
+    title: 'DOPE90',
+    subtitle: 'Lo-Head 2',
+    image: 'https://i.postimg.cc/yNGs1Zp3/dope90.jpg',
+    link: 'https://open.spotify.com/album/2QmWYW1527fmjDl9LUHAG1?si=SeYBeu1iT52Y8yqib2st-g',
+  },
+  {
+    title: 'FloFilz',
+    subtitle: 'Speakthru',
+    image: 'https://i.postimg.cc/6pYBPSd0/flofilz.jpg',
+    link: 'https://open.spotify.com/album/4kYA3PqnQ9nKhMtREJqW0i?si=6RMgKTCXRKyfUpGK70Klmw',
+  },
+  {
+    title: 'Nujabes',
+    subtitle: 'Metaphorical Music',
+    image: 'https://i.postimg.cc/t4XX2yVJ/nujabes.jpg',
+    link: 'https://open.spotify.com/album/5FrjDW96mCYw9ECc74c637?si=q5CSP-HbSUurTIVVfNQ-wQ',
+  },
+];
---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.header__button');
+const title = document.querySelector('.intro__title');
+const subtitle = document.querySelector('.intro__paragraph');
+const image = document.querySelector('.intro__image');
+const link = document.querySelector('.intro__link');
+
+function getRandomElement(arr) {
+  let randIndex = Math.floor(Math.random() * arr.length);
+  return arr[randIndex];
+};
+
+button.addEventListener('click', function () {
+  let randomElement = getRandomElement(albumCovers);
+  title.textContent = randomElement.title;
+  subtitle.textContent = randomElement.subtitle;
+  image.src = randomElement.image;
+  link.src = randomElement.link;
+});
